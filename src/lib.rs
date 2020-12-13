20e4f5f5b91bc81d7b0e43d82694d45421f2955a
John Sloboda <sloboda@gmail.com>
2020-12-13 14:09:18 -0500

    day 1 part 2

--- src/lib.rs
+++ src/lib.rs
@@ -11,10 +11,43 @@ pub fn read_i64_to_vec(filename: &str) -> Vec<i64> {
 
 }
 
+pub mod algo {
+
+pub fn binary_search(v: &Vec<i64>, q: i64, l: usize, h: usize) -> bool {
+    if v.is_empty() {
+        false
+    } else if l + 1 == h {
+        v[l] == q
+    } else {
+        let m = l + (h - l) / 2;
+        binary_search(v, q, l, m) || binary_search(v, q, m, h)
+    }
+}
+
+}
+
 #[cfg(test)]
 mod tests {
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn binary_search_tests() {
+        use super::algo::binary_search;
+
+        // not found
+        assert!(!binary_search(&vec![], 42, 0, 0));
+        assert!(!binary_search(&vec![43], 42, 0, 1));
+        assert!(!binary_search(&vec![41, 43], 42, 0, 2));
+        assert!(!binary_search(&vec![41, 43, 44], 42, 0, 3));
+        assert!(!binary_search(&vec![41, 43, 44, 45], 42, 0, 4));
+
+        // found
+        assert!(binary_search(&vec![42], 42, 0, 1));
+        assert!(binary_search(&vec![42], 42, 0, 1));
+        assert!(binary_search(&vec![41, 42], 42, 0, 2));
+        assert!(binary_search(&vec![42, 43], 42, 0, 2));
+        assert!(binary_search(&vec![41, 42, 44], 42, 0, 3));
+        assert!(binary_search(&vec![40, 41, 42, 44], 42, 0, 4));
+        assert!(binary_search(&vec![41, 42, 43, 44], 42, 0, 4));
     }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 mod lib;
 
+use crate::lib::parse;
+
 mod expenses {
 
-use crate::lib::parse;
 use std::collections::HashSet;
 
-pub fn sum_to_target_set(filename: &str, target: i64) -> Option<i64> {
-    let nums = parse::read_i64_to_vec(filename);
+pub fn two_sum_to_target<'a, T: IntoIterator<Item=&'a i64>>(nums: T, target: i64) -> Option<i64> {
     let mut nums_seen = HashSet::new();
-    for n in nums {
+    for n in nums.into_iter() {
         let complement = target - n;
         if nums_seen.contains(&complement) {
             return Some(n * complement);
@@ -18,11 +18,40 @@ pub fn sum_to_target_set(filename: &str, target: i64) -> Option<i64> {
     None
 }
 
+pub fn three_sum_to_target(nums: &Vec<i64>, target: i64) -> Option<i64> {
+    if nums.len() >= 3 {
+        let mut n = nums.clone();
+        n.sort();
+        let (mut l, mut h) = (0, n.len() - 1);
+        while l < h {
+            if let Some(complement_product) = two_sum_to_target(&n[l..h], target - n[h]) {
+                return Some(complement_product * n[h])
+            } else {
+                h -= 1;
+            }
+            if let Some(complement_product) = two_sum_to_target(&n[l+1..h+1], target - n[l]) {
+                return Some(complement_product * n[l])
+            } else {
+                l += 1;
+            }
+        }
+        None
+    } else {
+        None
+    }
+}
+
 }
 
 fn main() {
-    if let Some(product) = expenses::sum_to_target_set("input/01.txt", 2020) {
+    let nums = parse::read_i64_to_vec("input/01.txt");
+    if let Some(product) = expenses::two_sum_to_target(&nums, 2020) {
         println!("part 1, product: {}", product);
+    } else {
+        println!("no product found");
+    }
+    if let Some(product) = expenses::three_sum_to_target(&nums, 2020) {
+        println!("part 2, product: {}", product);
     } else {
         println!("no product found");
     }

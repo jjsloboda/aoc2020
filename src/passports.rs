e6452f514c0e987fef33e9ca269316fdf5609cd2
John Sloboda <sloboda@gmail.com>
2020-12-13 23:48:43 -0500

    checkpoint day 4 part 1

--- src/passports.rs
+++ src/passports.rs
@@ -1,7 +1,23 @@
 
+use std::collections::HashSet;
+
+use regex::Regex;
 
 use crate::lib;
 
 pub fn run(part: u32) {
-    let lines = lib::parse::read_to_vec::<String>("input/04.txt");
+    let lines: Vec<String> = lib::parse::read_to_vec::<String>("input/04.txt")
+        .split(|s| s.is_empty())
+        .map(|l| l.join(" "))
+        .collect();
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"(?:(?P<key>[[:lower:]]{3}):(?P<value>.+) ?)+").unwrap();
+        static ref required_keys: HashSet<&'static str> = HashSet::from_iter(
+            IntoIter::new(["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"]));
+    }
+    let mut num_valid = 0;
+    for l in lines {
+        let keys = RE.captures_iter(l).map(|g| &g["key"]).collect();
+    }
+    println!("num valid: {}", num_valid);
 }

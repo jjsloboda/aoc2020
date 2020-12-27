d4d406dcdb2b400553fc36ddf9a5161961c52ccf
John Sloboda <sloboda@gmail.com>
2020-12-26 23:31:02 -0500

    day 5 part 2

--- src/boarding.rs
+++ src/boarding.rs
@@ -3,7 +3,7 @@
 use crate::lib;
 
 pub fn run(part: u32) {
-    let greatest: u64 = lib::parse::read_to_vec::<String>("input/05.txt")
+    let mut seat_ids: Vec<_> = lib::parse::read_to_vec::<String>("input/05.txt")
         .iter()
         .map(|s| {
             let b = s
@@ -13,7 +13,18 @@ pub fn run(part: u32) {
                 .replace("R", "1");
             u64::from_str_radix(&b, 2).expect("couldn't parse binary")
         })
-        .max()
-        .expect("no elements in input");
-    println!("greatest boarding pass: {}", greatest);
+        .collect();
+    if part == 1 {
+        let greatest = seat_ids.iter().max().expect("no elements in input");
+        println!("greatest boarding pass: {}", greatest);
+    } else if part == 2 {
+        seat_ids.sort();
+        for i in 0..seat_ids.len()-1 {
+            let (s1, s2) = (seat_ids[i], seat_ids[i+1]);
+            if s2 - s1 == 2 {
+                println!("my seat: {}", s1 + 1);
+                return;
+            }
+        }
+    }
 }

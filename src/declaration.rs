e324707ccc277f2b3c1c8d0de3145f3981fa2c01
John Sloboda <sloboda@gmail.com>
2020-12-27 14:19:28 -0500

    day 6 part 2

--- src/declaration.rs
+++ src/declaration.rs
@@ -1,19 +1,35 @@
 
+use std::collections::HashMap;
+
+use counter::Counter;
 
 use crate::lib;
 
 pub fn run(part: u32) {
     let lines: Vec<String> = lib::parse::read_to_vec::<String>("input/06.txt")
         .split(|s| s.is_empty())
-        .map(|l| l.join(""))
+        .map(|l| l.join(" "))
         .collect();
-    let total: u64 = lines.iter()
-        .map(|l| {
-            let mut chars = l.chars().collect::<Vec<_>>();
-            chars.sort_unstable();
-            chars.dedup();
-            chars.len() as u64
-        })
-        .sum();
-    println!("total questions with yes: {}", total);
+    if part == 1 {
+        let total: u64 = lines.iter()
+            .map(|l| {
+                let mut chars = l.chars().collect::<Vec<_>>();
+                chars.sort_unstable();
+                chars.dedup();
+                (if l.contains(" ") { chars.len() - 1 } else { chars.len() }) as u64
+            })
+            .sum();
+        println!("total questions with one yes: {}", total);
+    } else if part == 2 {
+        let total: u64 = lines.iter()
+            .map(|l| {
+                let counter = l.chars().collect::<Counter<_>>();
+                let num_ppl = counter[&' '] + 1;
+                counter.values()
+                    .map(|&v| if v == num_ppl {1} else {0})
+                    .sum::<u64>()
+            })
+            .sum();
+        println!("total questions with all yes: {}", total);
+    }
 }

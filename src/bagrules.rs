cd543bcff18e5f4a0e38dabdb10a831a0cc4a4e5
John Sloboda <sloboda@gmail.com>
2020-12-30 21:03:36 -0500

    use fromstr trait for day 7

--- src/bagrules.rs
+++ src/bagrules.rs
@@ -1,7 +1,8 @@
 
 use std::collections::{HashSet, HashMap, VecDeque};
+use std::str::FromStr;
 
-use regex::Regex;
+use regex::{Regex, Error as RegexError};
 use string_cache::DefaultAtom as Atom;
 
 use crate::lib;
@@ -12,38 +13,45 @@ struct Rule {
 }
 
 impl Rule {
-    pub fn from(s: &str) -> Self {
+}
+
+impl FromStr for Rule {
+    type Err = RegexError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+
+        fn split_colors(s: &str) -> Vec<(Atom, u32)> {
+            if s == "no other bags" {
+                vec![]
+            } else {
+                s.split(", ")
+                    .map(|c| {
+                        lazy_static! {
+                            static ref RE: Regex = Regex::new(
+                                r"^([0-9]+) ([a-z]+ [a-z]+) bags?$").unwrap();
+                        }
+                        let caps = RE.captures(c).expect("bad color contains list");
+                        (Atom::from(&caps[2]), caps[1].parse::<u32>().expect("bad input qty"))
+                    })
+                    .collect()
+            }
+        }
+
         lazy_static! {
             static ref RE: Regex = Regex::new(
                 r"^([a-z]+ [a-z]+) bags contain ([ ,a-z0-9]+)\.$").unwrap();
         }
-        let caps = RE.captures(s).expect("malformed input");
-        Self{ color: Atom::from(&caps[1]), colors_contained: Rule::split_colors(&caps[2]) }
-    }
-
-    fn split_colors(s: &str) -> Vec<(Atom, u32)> {
-        if s == "no other bags" {
-            vec![]
+        if let Some(caps) = RE.captures(s) {
+            Ok(Self{ color: Atom::from(&caps[1]), colors_contained: split_colors(&caps[2]) })
         } else {
-            s.split(", ")
-                .map(|c| {
-                    lazy_static! {
-                        static ref RE: Regex = Regex::new(
-                            r"^([0-9]+) ([a-z]+ [a-z]+) bags?$").unwrap();
-                    }
-                    let caps = RE.captures(c).expect("bad color contains list");
-                    (Atom::from(&caps[2]), caps[1].parse::<u32>().expect("bad input qty"))
-                })
-                .collect()
+            Err(RegexError::Syntax(String::from("malformed input line")))
         }
     }
+
 }
 
 pub fn run(part: u32) {
-    let rules: Vec<Rule> = lib::parse::read_to_vec::<String>("input/07.txt")
-        .iter()
-        .map(|l| Rule::from(l))
-        .collect();
+    let rules: Vec<Rule> = lib::parse::read_to_vec::<Rule>("input/07.txt");
 
     if part == 1 {
         let contains: HashMap<_, _> = rules.iter()

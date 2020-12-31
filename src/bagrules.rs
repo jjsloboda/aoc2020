f287611e87bb9bdc966d1c681215caed3a47c8db
John Sloboda <sloboda@gmail.com>
2020-12-30 19:23:00 -0500

    day 7 part 2

--- src/bagrules.rs
+++ src/bagrules.rs
@@ -8,7 +8,7 @@ use crate::lib;
 
 struct Rule {
     color: Atom,
-    colors_contained: Vec<Atom>,
+    colors_contained: Vec<(Atom, u32)>,
 }
 
 impl Rule {
@@ -21,7 +21,7 @@ impl Rule {
         Self{ color: Atom::from(&caps[1]), colors_contained: Rule::split_colors(&caps[2]) }
     }
 
-    fn split_colors(s: &str) -> Vec<Atom> {
+    fn split_colors(s: &str) -> Vec<(Atom, u32)> {
         if s == "no other bags" {
             vec![]
         } else {
@@ -32,7 +32,7 @@ impl Rule {
                             r"^([0-9]+) ([a-z]+ [a-z]+) bags?$").unwrap();
                     }
                     let caps = RE.captures(c).expect("bad color contains list");
-                    Atom::from(&caps[2])
+                    (Atom::from(&caps[2]), caps[1].parse::<u32>().expect("bad input qty"))
                 })
                 .collect()
         }
@@ -44,18 +44,39 @@ pub fn run(part: u32) {
         .iter()
         .map(|l| Rule::from(l))
         .collect();
-    let contains: HashMap<_, _> = rules.iter()
-        .map(|r| (r.color.clone(), r.colors_contained.clone()))
-        .collect();
-    let within = lib::algo::invert_hashmap(contains);
 
-    let mut seen = HashSet::new();
-    let mut q = VecDeque::new();
-    q.push_back(Atom::from("shiny gold"));
-    while let Some(color) = q.pop_front() {
-        if seen.contains(&color) { continue; }
-        q.extend(if let Some(colors) = within.get(&color) { colors.clone() } else { vec![] });
-        seen.insert(color);
+    if part == 1 {
+        let contains: HashMap<_, _> = rules.iter()
+            .map(|r| (r.color.clone(), r.colors_contained.iter().map(|v| v.0.clone()).collect()))
+            .collect();
+        let within = lib::algo::invert_hashmap(contains);
+        let mut seen = HashSet::new();
+        let mut q = VecDeque::new();
+        q.push_back(Atom::from("shiny gold"));
+        while let Some(color) = q.pop_front() {
+            if seen.contains(&color) { continue; }
+            q.extend(if let Some(colors) = within.get(&color) { colors.clone() } else { vec![] });
+            seen.insert(color);
+        }
+        println!("number of colors that can contain shiny gold: {}", seen.len() - 1);
+    } else if part == 2 {
+        let contains_qty: HashMap<_, _> = rules.iter()
+            .map(|r| (r.color.clone(), r.colors_contained.clone()))
+            .collect();
+        let mut total_bags = 0;
+        let mut q = VecDeque::new();
+        q.push_back((Atom::from("shiny gold"), 1));
+        while let Some((color, qty)) = q.pop_front() {
+            q.extend(
+                if let Some(colors_qtys) = contains_qty.get(&color) {
+                    colors_qtys.iter()
+                        .map(|(c, q)| (c.clone(), q * qty))
+                        .collect()
+                } else {
+                    vec![]
+                });
+            total_bags += qty;
+        }
+        println!("number of bags inside a shiny gold: {}", total_bags - 1);
     }
-    println!("number of colors that can contain shiny gold: {}", seen.len() - 1);
 }

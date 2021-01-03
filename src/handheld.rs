c523ab75a5722da671552c89cdb13b215aff306a
John Sloboda <sloboda@gmail.com>
2021-01-02 20:26:18 -0500

    day 8 part 2

--- src/handheld.rs
+++ src/handheld.rs
@@ -6,8 +6,9 @@ use std::str::FromStr;
 
 use crate::lib;
 
+#[derive(Clone)]
 enum Instruction {
-    NOP,
+    NOP(isize),
     ACC(isize),
     JMP(isize),
 }
@@ -49,7 +50,7 @@ impl FromStr for Instruction {
         if tokens.len() != 2 { return Err(BadInputLine.into()); }
         let param = tokens[1].parse::<isize>()?;
         match tokens[0] {
-            "nop" => Ok(Instruction::NOP),
+            "nop" => Ok(Instruction::NOP(param)),
             "acc" => Ok(Instruction::ACC(param)),
             "jmp" => Ok(Instruction::JMP(param)),
             _ => Err(UnknownCode.into()),
@@ -61,7 +62,7 @@ fn execute(state: &mut State, prgm: &Vec<Instruction>) {
     assert!(state.pc >= 0 && state.pc < prgm.len() as isize,
         "state.pc out of bounds: {}, prgm len {}", state.pc, prgm.len());
     match prgm[state.pc as usize] {
-        Instruction::NOP => state.pc += 1,
+        Instruction::NOP(_) => state.pc += 1,
         Instruction::ACC(x) => {
             state.acc += x;
             state.pc += 1;
@@ -70,13 +71,34 @@ fn execute(state: &mut State, prgm: &Vec<Instruction>) {
     }
 }
 
-pub fn run(part: u32) {
-    let program: Vec<Instruction> = lib::parse::read_to_vec::<Instruction>("input/08.txt");
+fn run_to_termination(prgm: &Vec<Instruction>) -> State {
     let mut state = State::new();
     let mut seen = HashSet::new();
-    while !seen.contains(&state.pc) {
+    while !seen.contains(&state.pc) && state.pc != prgm.len() as isize {
         seen.insert(state.pc);
-        execute(&mut state, &program);
+        execute(&mut state, &prgm);
     }
-    println!("accumulator: {}", state.acc);
+    state
+}
+
+pub fn run(part: u32) {
+    let program: Vec<Instruction> = lib::parse::read_to_vec::<Instruction>("input/08.txt");
+    if part == 1 {
+        let state = run_to_termination(&program);
+        println!("accumulator: {}", state.acc);
+    } else if part == 2 {
+        for i in 0..program.len() {
+            let mut trial_prgm = program.clone();
+            trial_prgm[i] = match trial_prgm[i] {
+                Instruction::NOP(x) => Instruction::JMP(x),
+                Instruction::ACC(x) => Instruction::ACC(x),
+                Instruction::JMP(x) => Instruction::NOP(x),
+            };
+            let state = run_to_termination(&trial_prgm);
+            if state.pc == trial_prgm.len() as isize {
+                println!("accumulator: {}", state.acc);
+                break;
+            }
+        }
+    }
 }
